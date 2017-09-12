667cef0bc149778cd6a97a253de04c03aaebfc02
STENUNGSUND\rebecca.leckborn <rebecca.leckborn@hotmail.com>
2017-09-12 09:13:29 +0200

    Added Oscar's "Timer"

--- Program.cs
+++ Program.cs
@@ -10,6 +10,53 @@ namespace Charader
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Run();
+        }
+
+        bool guessingIsActive;
+
+
+        public void Run()
+        {
+            guessingIsActive = true;
+            Console.WriteLine("*** ROUND 1 ***");
+            StopAfterThreeSeconds();
+            WordLoop();
+
+            guessingIsActive = true;
+            Console.WriteLine("*** ROUND 2 ***");
+            StopAfterThreeSeconds();
+            WordLoop();
+        }
+
+        private async void StopAfterThreeSeconds()
+        {
+            await Task.Delay(3000);
+            StopCurrentRound();
+        }
+
+        private void StopCurrentRound()
+        {
+            Console.WriteLine("Stop current round!");
+            Console.WriteLine();
+            guessingIsActive = false;
+        }
+
+        public void WordLoop()
+        {
+            while (true)
+            {
+                if (!guessingIsActive)
+                {
+                    break;
+                }
+                if (Console.KeyAvailable)
+                {
+                    Console.WriteLine("New random word");
+                    Console.ReadKey(true);
+                }
+            }
         }
     }
 }
