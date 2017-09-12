ef350a9eb25c193deceb275958d16570a1ae8801
STENUNGSUND\rebecca.leckborn <rebecca.leckborn@hotmail.com>
2017-09-12 09:40:13 +0200

    Added CreateTeams

--- Program.cs
+++ Program.cs
@@ -11,23 +11,55 @@ namespace Charader
         static void Main(string[] args)
         {
             Program p = new Program();
-            p.Run();
+            p.Menu();
         }
 
         bool guessingIsActive;
 
+        private void Menu()
+        {
+            Console.WriteLine("-----");
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("-----");
+            Console.WriteLine("[1] Play Charuder");
+            Console.WriteLine("[2] Add new words");
+            //TODO: lägg in menyýval settings
+            Console.WriteLine("[3] Quit");
+
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 1:
+                    Run();
+                    break;
+                case 2:
+                    //AddNewWords();
+                    break;
+                case 7:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine();
+                    Console.WriteLine("Choose one of the alternatives in the menu!");
+                    break;
+            }
+        }
 
         public void Run()
         {
-            guessingIsActive = true;
-            Console.WriteLine("*** ROUND 1 ***");
-            StopAfterThreeSeconds();
-            WordLoop();
+            List<Team> teamList = CreateTeams();
 
-            guessingIsActive = true;
-            Console.WriteLine("*** ROUND 2 ***");
-            StopAfterThreeSeconds();
-            WordLoop();
+            //loop på antal lag = en omgång
+            foreach (var team in teamList)
+            {
+                Console.WriteLine($"*** {team.Name} ***");
+                Console.WriteLine("Press Enter to show word");
+                guessingIsActive = true;
+                StopAfterThreeSeconds();
+                WordLoop();
+            }
         }
 
         private async void StopAfterThreeSeconds()
@@ -53,10 +85,24 @@ namespace Charader
                 }
                 if (Console.KeyAvailable)
                 {
+
                     Console.WriteLine("New random word");
                     Console.ReadKey(true);
                 }
             }
+        }
+        private List<Team> CreateTeams()
+        {
+            Console.Write("How many teams are playing this awesome game? ");
+            int nrOfTeams = int.Parse(Console.ReadLine()); //TODO: på nåt sätt kolla så man skriver en siffra! Regex
+            List<Team> teamList = new List<Team>();
+
+            for (int i = 0; i < nrOfTeams; i++)
+            {
+                Console.WriteLine($"Name of team {i + 1}: ");
+                teamList.Add(new Team(Console.ReadLine()));
+            }
+            return teamList;
         }
     }
 }
